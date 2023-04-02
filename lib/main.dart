9c32288c49a6bae5031f432ddb5ef39349bc0d79
yacineboudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2023-04-02 18:47:13 +0100

    upload

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MySop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop'),
+      ),
+    );
+  }
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    required this.id,
+    required this.description,
+    required this.title,
+    required this.imageUrl,
+    required this.price,
+    this.isFavorite = false,
+  });
+}
