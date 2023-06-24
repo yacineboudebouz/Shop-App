8dac3bd4e48e346d857444d6f1b4e74797f69bb8
yacineboudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2023-06-24 11:51:13 +0100

    cart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
